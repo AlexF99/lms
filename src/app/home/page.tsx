229cbaaf57d1130ce3a920592bff11997ee32741
AlexF99 <alexandreopf@gmail.com>
2023-10-30 17:33:56 -0300

    added title to course schema

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,32 @@
-export default function Home() {
+import { authOptions } from "@/lib/auth"
+import prisma from "@/lib/db"
+import { getServerSession } from "next-auth"
+
+export default async function Home() {
+
+  const loggedUser = (await getServerSession(authOptions))?.user;
+
+  const myCourses = await prisma.course.findMany({
+    where: {
+      OR: [
+        { ownerId: loggedUser?.id },
+        {
+          students: {
+            some: {
+              id: loggedUser?.id
+            }
+          }
+        }
+      ]
+    }
+  })
+
+  console.log(myCourses)
 
   return (
-    <div>Hello World</div>
+    <div>
+      <h1 className='mr-3 text-xl'>My courses</h1>
+
+    </div>
   )
 }
