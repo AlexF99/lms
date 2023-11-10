6116b3900cf6be5b6822066a4e2c48157232937f
AlexF99 <alexandreopf@gmail.com>
2023-11-10 15:16:33 -0300

    querying and displaying questions & choices in quiz page

--- src/app/home/quiz/[id]/page.tsx
+++ src/app/home/quiz/[id]/page.tsx
@@ -1,4 +1,3 @@
-import Comments from "@/components/comments";
 import { authOptions } from "@/lib/auth";
 import prisma from "@/lib/db"
 import { getServerSession } from "next-auth";
@@ -10,16 +9,40 @@ export default async function Page({ params }: { params: { id: string } }) {
         const quiz = await prisma.quiz.findUniqueOrThrow({
             where: {
                 id: params.id
+            },
+            select: {
+                title: true,
+                Question: {
+                    select: {
+                        id: true,
+                        text: true,
+                        choice: true
+                    }
+                }
             }
         })
+
+        const questions = quiz.Question;
 
         if (!quiz) return <h1>Could not find quiz!</h1>
 
         return (
             <div>
                 <h1 className="text-xl text-center">{quiz?.title}</h1>
-                <div className="mt-3">
 
+                <div className="mt-3">
+                    <form>
+                        {questions && questions.map(q => (
+                            <div key={q.id}>
+                                {q.text}
+                                <ul>
+                                    {q.choice && q.choice.map(c => (
+                                        <li key={c.id}>{c.text}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                    </form>
                 </div>
             </div>
         )
