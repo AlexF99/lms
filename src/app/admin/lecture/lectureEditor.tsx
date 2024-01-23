5ff06fbad8078eabb8e7ae4bbf01b4590e8c6a46
Alexandre Freitas <alexandreopf@gmail.com>
2024-01-23 17:46:29 -0300

    moved quill editor out of its oun component

--- src/app/admin/lecture/lectureEditor.tsx
+++ src/app/admin/lecture/lectureEditor.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import RichEditor from "@/components/richEditor";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
@@ -10,10 +10,17 @@ interface lectureForm {
     media: string;
 }
 
+import dynamic from 'next/dynamic';
+import 'react-quill/dist/quill.snow.css'; // Import Quill styles
+
+const QuillEditor = dynamic(() => import('react-quill'), { ssr: false });
+
 export default function LectureEditor(props: any) {
     const { lectureToEdit, modules } = props;
     const { register, handleSubmit, setValue, getValues } = useForm<lectureForm>();
-    const [richText, setRichText] = useState('');
+    const initialContent = lectureToEdit ? lectureToEdit.richText : '';
+    const [richText, setRichText] = useState(initialContent);
+    const router = useRouter();
 
     useEffect(() => {
         if (lectureToEdit !== undefined && lectureToEdit !== null && modules && modules?.length) {
@@ -27,18 +34,52 @@ export default function LectureEditor(props: any) {
         if (lectureToEdit !== undefined && lectureToEdit !== null) {
             // edit
             const updated = { ...lecture, richText, id: lectureToEdit.id }
-            console.log(updated);
+            await (await fetch("/api/lecture",
+                {
+                    method: "PUT", headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ ...updated })
+                })).json();
         } else {
             // create
             const newLecture = { ...lecture, richText }
-            console.log(newLecture);
-            const res = await (await fetch("/api/lecture",
+            await (await fetch("/api/lecture",
                 {
                     method: "POST", headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ ...newLecture })
                 })).json();
         }
+        router.push("/admin/lecture")
     }
+
+
+    const quillModules = {
+        toolbar: [
+            [{ header: [1, 2, 3, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            ['link', 'image'],
+            [{ align: [] }],
+            [{ color: [] }],
+            ['code-block'],
+            ['clean'],
+        ],
+    };
+
+    const quillFormats = [
+        'header',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'blockquote',
+        'list',
+        'bullet',
+        'link',
+        'image',
+        'align',
+        'color',
+        'code-block',
+    ];
 
     return (
         <div>
@@ -49,7 +90,13 @@ export default function LectureEditor(props: any) {
                 </select>
                 <input type="text" {...register('title')} placeholder="Title" className="input  mb-2 input-bordered w-full" />
                 <input type="text" {...register('media')} placeholder="media" className="input  mb-2 input-bordered w-full" />
-                <RichEditor richtextChange={setRichText} content={lectureToEdit ? lectureToEdit.richText : ''} />
+                <QuillEditor
+                    value={richText}
+                    onChange={(newContent) => setRichText(newContent)}
+                    modules={quillModules}
+                    formats={quillFormats}
+                    className="w-full h-[70%] mt-10 bg-white"
+                />
                 <button className="btn btn-primary mt-2">submit</button>
             </form>
         </div>
