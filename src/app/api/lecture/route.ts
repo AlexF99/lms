142cfe36f3215af1677ead9adb715ffd3a4c66be
Alexandre Freitas <alexandreopf@gmail.com>
2024-01-23 17:46:01 -0300

    created PUT route for lecture editing

--- src/app/api/lecture/route.ts
+++ src/app/api/lecture/route.ts
@@ -14,7 +14,31 @@ export async function POST(req: Request) {
             }
         });
 
-        return NextResponse.json({ lecture: newLecture, message: "lecture saved" }, { status: 201 });
+        return NextResponse.json({ lecture: newLecture, message: "lecture created" }, { status: 201 });
+    } catch (err) {
+        console.log(err)
+        return NextResponse.json({ message: "could not save lecture" }, { status: 409 });
+    }
+}
+
+
+export async function PUT(req: Request) {
+    try {
+        const body = await req.json()
+        const { moduleId, title, richText, media, id } = body;
+        const newLecture = await prisma.lecture.update({
+            where: {
+                id: id
+            },
+            data: {
+                moduleId: moduleId,
+                title: title,
+                media: media,
+                richText: richText,
+            }
+        });
+
+        return NextResponse.json({ lecture: newLecture, message: "lecture updated" }, { status: 201 });
     } catch (err) {
         console.log(err)
         return NextResponse.json({ message: "could not save lecture" }, { status: 409 });
