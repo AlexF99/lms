ab63ea2eed47def3c714de95e06de6640301d98a
Alexandre Freitas <alexandreopf@gmail.com>
2024-01-30 20:06:27 -0300

    small UI adjustment in the course card

--- src/components/courseCard.tsx
+++ src/components/courseCard.tsx
@@ -15,8 +15,13 @@ export default async function CourseCard(props: any) {
     const course: Course = props.course;
 
     return (
-        <Link href={`/home/course/${course.id}`} className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={course.imageUrl && course.imageUrl.length ? course.imageUrl : "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"} alt="courseLogo" /></figure>
+        <Link href={`/home/course/${course.id}`} className="card w-96 bg-base-100 shadow-xl h-96">
+            <figure className="h-64">
+                <img src={course.imageUrl && course.imageUrl.length ? course.imageUrl : "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"}
+                    alt="courseLogo"
+                    className="object-cover"
+                />
+            </figure>
             <div className="card-body">
                 <h2 className="card-title">
                     {course.title}
